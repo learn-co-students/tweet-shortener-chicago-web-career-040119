989e32082978052ba2277d7434ff37c52034df79
Diana Utan <dia_utzi@yahoo.com>
2019-03-27 04:47:30 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,35 +5,51 @@ def word_substituter(tweet)
   short_tweet = []
   
     words.each do |word|
-     case word.downcase 
-     when "hello"
-       short_tweet << "hi"
-     when "to", "two", "too" 
+    case word.downcase 
+    when "hello"
+      short_tweet << "hi"
+    when "to", "two", "too" 
         short_tweet << "2"
-     when "for", "four" 
+    when "for", "four" 
       short_tweet << "4"
-     when "be" 
+    when "be" 
       short_tweet << "b"
-     when "you"
-       short_tweet << "u"
-     when "at"
+    when "you"
+      short_tweet << "u"
+    when "at"
         short_tweet << "@"
-     when "and"
+    when "and"
         short_tweet << "&"
-     else
+    else
         short_tweet << word 
     end
   end
     short_tweet.join(" ")
   end
+  
+                    # shorter version 
+  # def word_substituter(tweet)
+#   array = tweet.split(" ")
+#   array.each_with_index do |word, index|
+#     dictionary.each do |key, value|
+#       if word == key
+#         array[index] = value
+#       end
+#     end
+#   end
+#   array.join(" ")
+# end
+  
              
- def bulk_tweet_shortener(tweets)
-   tweets.each do |tweet|
-     tweet = word_substituter(tweet)
-     puts "#{tweet}"
-   end 
- end
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    tweet = word_substituter(tweet)
+    puts "#{tweet}"
+  end 
+end
  
+
+
  def selective_tweet_shortener(tweets)
      if tweets.length > 140
        word_substituter(tweets)
